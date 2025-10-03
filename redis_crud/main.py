a7ace36db9bb2efb3ed097265cd2c1be733e3950
VishnuRathore98 <rathorevishnu666@gmail.com>
2025-10-03 23:13:44 +0530

    addrd redis crud

--- redis_crud/main.py
+++ redis_crud/main.py
@@ -1,12 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from pydantic import UUID4
 from redis_om import get_redis_connection, HashModel
 
 redis = get_redis_connection(
-    host="",
-    port="",
-    password="",
-    decode_resposes=True
+    host="localhost",
+    port="6379",
+    decode_responses=True
 )
 
 app = FastAPI()
@@ -21,22 +20,39 @@ class Product(HashModel):
         database = redis
 
 
-@app.get("/get_product/{id}")
+@app.get("/get/{id}")
 def get_product(id: UUID4):
-    pass
+    product = Product.get(id)
+    if not product:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product Not found")
+    return product.model_dump()
 
-@app.post("create_product")
+@app.get("/get")
+def get_products():
+    products=[]
+    for pk in Product.all_pks():
+        p = Product.get(pk)
+        products.append(p.model_dump())
+    return products
+
+
+@app.post("/create")
 def create_product(product: Product):
-    pass
+    product = Product(**product.model_dump())
+    product.save()
+    return {"id":product.pk}
 
-@app.put("update_product/{id}")
+@app.put("/update/{id}")
 def update_product(product: Product):
     pass
 
-@app.patch("patch_product/{id}")
+@app.patch("/patch/{id}")
 def patch_product():
     pass
 
-@app.delete("delete_product/{id}")
+@app.delete("/delete/{id}")
 def delete_product(id:UUID4):
-    pass
+    Product.delete(id)
+    return {"message": "product deleted successfully."}
+
+
